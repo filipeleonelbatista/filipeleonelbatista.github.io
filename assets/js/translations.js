8051174ddbf526a5fb07761453f79176d807c749
Filipe de Leonel Batista <filipe.x@live.com>
2025-06-23 18:22:15 -0300

    ajuste link github

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -28,13 +28,15 @@ export const translations = {
     'youtube-title': 'Canal no Youtube',
 
     // Footer
-    'footer': '© 2024 Filipe de Leonel Batista. Todos os direitos reservados.',
+    'footer': 'Filipe de Leonel Batista. Todos os direitos reservados.',
 
     // URLs
     'linkedin-url': 'https://linkedin.com/in/filipeleonelbatista',
     'email-url': "mailto:filipe.x2016@gmail.com?subject=Ol%C3%A1%20Filipe%2C%20gostaria%20de%20conversar%20sobre%20um%20projeto&body=Ol%C3%A1%20Filipe%2C%0A%0AGostaria%20de%20conversar%20sobre%20um%20projeto.%20Podemos%20agendar%20uma%20reuni%C3%A3o%3F%0A%0AAtenciosamente%2C%0A",
     'whatsapp-url': "https://wa.me/+5551992736445?text=Ol%C3%A1%20Filipe%2C%0A%0AGostaria%20de%20conversar%20sobre%20um%20projeto.%20Podemos%20agendar%20uma%20reuni%C3%A3o%3F%0A%0AAtenciosamente.",
-    'curriculo-url': './Curriculo.pdf'
+    'curriculo-url': './Curriculo.pdf',
+    'github-url': 'https://github.com/filipeleonelbatista',
+    
   },
   en: {
     // Header e Botões
@@ -65,13 +67,14 @@ export const translations = {
     'youtube-title': 'YouTube Channel',
 
     // Footer
-    'footer': '© 2024 Filipe de Leonel Batista. All rights reserved.',
+    'footer': 'Filipe de Leonel Batista. All rights reserved.',
 
     // URLs
     'linkedin-url': 'https://www.linkedin.com/in/filipeleonelbatista/?locale=en_US',
     'email-url': "mailto:filipe.x2016@gmail.com?subject=Hi%20Filipe%2C%20I%20would%20like%20to%20discuss%20a%20project&body=Hi%20Filipe%2C%0A%0AI%20would%20like%20to%20discuss%20a%20project%20with%20you.%20Can%20we%20schedule%20a%20meeting%3F%0A%0ABest%20regards%2C%0A",
     'whatsapp-url': "https://wa.me/+5551992736445?text=Hi%20Filipe%2C%0A%0AI%20would%20like%20to%20discuss%20a%20project%20with%20you.%20Can%20we%20schedule%20a%20meeting%3F%0A%0ABest%20regards.",
-    'curriculo-url': './Resume.pdf'
+    'curriculo-url': './Resume.pdf',    
+    'github-url': 'https://github.com/filipeleonelbatista/filipeleonelbatista/blob/master/README_EN.md',
   }
 };
 
